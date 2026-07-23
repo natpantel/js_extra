b41e3945460453582a7c857497f964dbffcd99a7
Mona Lisa~ <pantelnn@gmail.com>
2026-07-23 09:48:51 +0300

    Добавлено исправление

--- 3-birthday/birtday.js
+++ 3-birthday/birtday.js
@@ -1,11 +1,7 @@
-// Сделать функцию валидации возраста. На вход передаётся строка даты рождения пользователя вида “2022-01-01”.
-// Необходимо вывести true, если ему больше 14 лет и false, если меньше
-
 function validateBirthday(birthday) {
     const date = new Date(birthday);
     const today = new Date();
-    //console.log((today-date) / (1000 * 60 * 60 * 24 * 30 * 12))
-    const yearDif = today.getYear() - date.getYear();
+    const yearDif = today.getFullYear() - date.getFullYear();
     if (yearDif > 14) {
         return true
     } else if (yearDif == 14) {
